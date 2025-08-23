8f707a2e5552dc82e122a1fb9f190ebd0ab9a363
Zi Yong <yonglzy1611@gmail.com>
2025-08-23 20:50:00 +0800

    Add list

--- src/main/java/Noah.java
+++ src/main/java/Noah.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Noah {
     public static void main(String[] args) {
+        String[] list = new String[100];
+        int count = 0;
         Scanner sc = new Scanner(System.in);
 
         printLine();
@@ -10,17 +12,26 @@ public class Noah {
         printLine();
 
         while (true) {
-            String input = sc.nextLine();
+            String input = sc.nextLine().trim();
             if (input.equalsIgnoreCase("bye")) {
                 printLine();
                 System.out.println("Bye. Hope to see you again soon!");
                 printLine();
                 break;
-            }
+            } else if (input.equals("list")) {
+                printLine();
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+                printLine();
+            } else {
+                list[count] = input;
+                count++;
 
-            printLine();
-            System.out.println(" " + input);
-            printLine();
+                printLine();
+                System.out.println("added: " + input);
+                printLine();
+            }
         }
 
         sc.close();
