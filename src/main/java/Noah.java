d368a497db31cbf385675ab8882531fef6a1cee1
Zi Yong <yonglzy1611@gmail.com>
2025-08-23 20:37:44 +0800

    Add support for echo

--- src/main/java/Noah.java
+++ src/main/java/Noah.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
+
 public class Noah {
     public static void main(String[] args) {
-        System.out.println("____________________________________");
+        Scanner sc = new Scanner(System.in);
+
+        printLine();
         System.out.println("Hello! I'm Noah");
         System.out.println("What can I do for you?");
+        printLine();
+
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                printLine();
+                System.out.println("Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            }
+
+            printLine();
+            System.out.println(" " + input);
+            printLine();
+        }
+
+        sc.close();
+    }
+
+    private static void printLine() {
         System.out.println("____________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
