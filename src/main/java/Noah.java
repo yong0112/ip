5cc3ff8696a82b2c0a4ec155e7ce849ffbc16df9
Zi Yong <yonglzy1611@gmail.com>
2025-08-23 20:54:43 +0800

    Add Task Class

--- src/main/java/Noah.java
+++ src/main/java/Noah.java
@@ -40,4 +40,28 @@ public class Noah {
     private static void printLine() {
         System.out.println("____________________________________");
     }
+
+    public class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        public void markAsDone() {
+            this.isDone = true;
+        }
+
+        @Override
+        public String toString() {
+            return "[" + this.getStatusIcon() + "] " + this.description;
+        }
+    }
+
 }
