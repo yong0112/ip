7dcddf51e8f23ebcc0085a1f0a43e4ccb3c3350b
Zi Yong <yonglzy1611@gmail.com>
2025-08-23 19:54:46 +0800

    Add greeting and rename

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Noah User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Noah.java
@@ -0,0 +1,9 @@
+public class Noah {
+    public static void main(String[] args) {
+        System.out.println("____________________________________");
+        System.out.println("Hello! I'm Noah");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
