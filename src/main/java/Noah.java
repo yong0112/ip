2c296508d7543c2051511791bff7c2f2a1459e62
Zi Yong <yonglzy1611@gmail.com>
2025-08-23 21:20:24 +0800

    Add ability to mark and unmark tasks

--- src/main/java/Noah.java
+++ src/main/java/Noah.java
@@ -1,8 +1,9 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Noah {
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] tasks = new Task[100];
         int count = 0;
         Scanner sc = new Scanner(System.in);
 
@@ -18,14 +19,41 @@ public class Noah {
                 System.out.println("Bye. Hope to see you again soon!");
                 printLine();
                 break;
+            } else if (input.toLowerCase().startsWith("mark")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (index >= 0 && index < count) {
+                    tasks[index].markAsDone();
+                    printLine();
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("  " + tasks[index]);
+                    printLine();
+                } else {
+                    printLine();
+                    System.out.println("Oops! I can't find the task");
+                    printLine();
+                }
+            } else if (input.toLowerCase().startsWith("unmark")) {
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                if (index >= 0 && index < count) {
+                    tasks[index].unmark();
+                    printLine();
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("  " + tasks[index]);
+                    printLine();
+                } else {
+                    printLine();
+                    System.out.println("Oops! I can't find the task");
+                    printLine();
+                }
             } else if (input.equals("list")) {
                 printLine();
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < count; i++) {
-                    System.out.println((i + 1) + ". " + list[i]);
+                    System.out.println((i + 1) + ". " + tasks[i]);
                 }
                 printLine();
             } else {
-                list[count] = input;
+                tasks[count] = new Task(input);
                 count++;
 
                 printLine();
@@ -41,7 +69,7 @@ public class Noah {
         System.out.println("____________________________________");
     }
 
-    public class Task {
+    static class Task {
         protected String description;
         protected boolean isDone;
 
@@ -56,6 +84,10 @@ public class Noah {
 
         public void markAsDone() {
             this.isDone = true;
+        }
+
+        public void unmark() {
+            this.isDone = false;
         }
 
         @Override
