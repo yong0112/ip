530740a142a66deb9ad9acc022cc1b88dffe5aa7
Zi Yong <yonglzy1611@gmail.com>
2025-09-13 17:03:34 +0800

    Add support for Java Streams

--- src/main/java/noah/command/FindCommand.java
+++ src/main/java/noah/command/FindCommand.java
@@ -8,9 +8,17 @@ import noah.task.TaskList;
 import noah.ui.UI;
 import noah.util.Storage;
 
+/**
+ * Represents a command that searches for tasks containing a specified keyword.
+ */
 public class FindCommand extends Command {
     private final String keyword;
 
+    /**
+     * Constructs a FindCommand with the specified search keyword.
+     *
+     * @param keyword The keyword eventEndTime search for in task descriptions.
+     */
     public FindCommand(String keyword) {
         this.keyword = keyword;
     }
