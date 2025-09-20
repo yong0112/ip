903d3bfe21a203285bac1a20aa520d7360c1da70
Zi Yong <yonglzy1611@gmail.com>
2025-09-20 18:57:19 +0800

    Add more test case

--- src/test/java/noah/util/StorageTest.java
+++ src/test/java/noah/util/StorageTest.java
@@ -1,20 +1,23 @@
 package noah.util;
 
-import noah.exception.NoahException;
-import noah.task.Deadline;
-import noah.task.Event;
-import noah.task.Task;
-import noah.task.Todo;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import noah.exception.NoahException;
+import noah.task.Deadline;
+import noah.task.Event;
+import noah.task.Task;
+import noah.task.Todo;
 
 public class StorageTest {
     private Path tempFile;
@@ -33,9 +36,9 @@ public class StorageTest {
 
     @Test
     public void loadTasks_validTask_success() throws IOException, NoahException {
-        String content = "T | 1 | read book\n" +
-                "D | 0 | submit report | 2025-09-02 1800\n" +
-                "E | 0 | project meeting | Sep 01 2028 0000-Oct 01 2028 1600\n";
+        String content = "T | 1 | read book\n"
+                + "D | 0 | submit report | 2025-09-02 1800\n"
+                + "E | 0 | project meeting | Sep 01 2028 0000-Oct 01 2028 1600\n";
         Files.write(tempFile, content.getBytes());
 
         List<Task> tasks = storage.loadTasks();
@@ -46,9 +49,9 @@ public class StorageTest {
     }
 
     @Test
-    public void loadTasks_corruptedLine_skipped() throws Exception {
-        String content = "X | 0 | invalid line\n" +  // unknown type
-                "T | 1 | good task\n";
+    public void loadTasks_corruptedLine_skipped() throws IOException, NoahException {
+        String content = "X | 0 | invalid line\n" // unknown type
+                + "T | 1 | good task\n";
         Files.write(tempFile, content.getBytes());
 
         List<Task> tasks = storage.loadTasks();
@@ -57,7 +60,7 @@ public class StorageTest {
     }
 
     @Test
-    public void addTask_writesToFile_success() throws Exception {
+    public void addTask_writesToFile_success() throws IOException {
         storage.addTask("T | 0 | new task");
         List<String> lines = Files.readAllLines(tempFile);
         assertEquals(1, lines.size());
@@ -65,7 +68,7 @@ public class StorageTest {
     }
 
     @Test
-    public void updateTask_changesCorrectLine_success() throws Exception {
+    public void updateTask_changesCorrectLine_success() throws IOException {
         Files.write(tempFile, List.of("T | 0 | old task"));
         storage.updateTask("T | 1 | updated task", 0);
         List<String> lines = Files.readAllLines(tempFile);
@@ -73,7 +76,7 @@ public class StorageTest {
     }
 
     @Test
-    public void deleteTask_removesCorrectLine_success() throws Exception {
+    public void deleteTask_removesCorrectLine_success() throws IOException {
         Files.write(tempFile, List.of("line1", "line2", "line3"));
         storage.deleteTask(1);
         List<String> lines = Files.readAllLines(tempFile);
